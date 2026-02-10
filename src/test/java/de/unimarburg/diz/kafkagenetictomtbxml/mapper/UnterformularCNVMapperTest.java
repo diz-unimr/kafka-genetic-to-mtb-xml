42a0c1a259220b1acc67aa193f728e8c5d392c77
Paul-Christian Volkmer <code@pcvolkmer.de>
2026-02-10 15:24:44 +0100

    fix: several issues in CNV mapping
    This fixes several issues in CNV mapping:
    
    * Result should use Onkostar property cataloge code, not just some random value with similar meaning
    * The required field for copy number is 'CNVTotalCNDouble' since at least version 2.12
    * Use comma in 'CNVTotalCNDouble' as presented in case export
    * check if the copy number is null

--- src/test/java/de/unimarburg/diz/kafkagenetictomtbxml/mapper/UnterformularCNVMapperTest.java
+++ src/test/java/de/unimarburg/diz/kafkagenetictomtbxml/mapper/UnterformularCNVMapperTest.java
@@ -8,10 +8,56 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UnterformularCNVMapperTest {
+
+    private UnterformularCNVMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        var hgncConfigProperties = new HgncConfigurationProperties();
+        hgncConfigProperties.setEnabled(false);
+
+        this.mapper = new UnterformularCNVMapper(hgncConfigProperties);
+    }
+
+    @ParameterizedTest(name = "Map CNV type ''{3}'' and Copy Number ''{2}'' based on ''{0}'' and ''{1}''")
+    @CsvSource({
+            "0.10,copy loss,'0,10',L",
+            "0.50,copy gain,'0,50',LLG",
+            "2.99,copy gain,'2,99',LLG",
+            "3.00,copy gain,'3,00',G",
+            "87.00,copy gain,'87,00',G",
+
+    })
+    void mappingShouldMapCnvTypeAndCopyNumber(
+            final String copyNumber,
+            final String variantEffect,
+            final String expectedCopyNumber,
+            final String expectedType
+    ) throws Exception {
+        var variant = createVariant("UNKNOWNGENEFORTEST");
+        variant.setCopyNumber(copyNumber);
+        variant.setVariantEffect(variantEffect);
+
+        var actual = mapper.createXmlUnterformularCNV(
+                defaultMtbPatientInfo(),
+                variant,
+                defaultDokumentierendeFachabteilung(),
+                1
+        );
+
+        var copyNumberVariation = actual.getEintraege().stream().filter(eintrag -> eintrag.getFeldname().equals("CopyNumberVariation")).findFirst();
+        var cnvTotalCNDouble = actual.getEintraege().stream().filter(eintrag -> eintrag.getFeldname().equals("CNVTotalCNDouble")).findFirst();
+
+        assertThat(copyNumberVariation).isPresent().hasValueSatisfying(eintrag -> assertThat(eintrag.getWert()).isEqualTo(expectedType));
+        assertThat(cnvTotalCNDouble).isPresent().hasValueSatisfying(eintrag -> assertThat(eintrag.getWert()).isEqualTo(expectedCopyNumber));
+
+    }
 
     @Nested
     @DisplayName("Tests with disabled HGNC list")
